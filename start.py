e4c8600b797bc3b5a8339b4f60b0fb5a56bb0274
ArtParse <artparse90@gmail.com>
2025-02-05 18:30:48 +0100

    dynamic project sys path

--- start.py
+++ start.py
@@ -1,3 +1,17 @@
 from ZEGGS.server import run
 
+
+import os
+import sys
+from pathlib import Path
+
+current_file = Path(__file__).resolve()
+
+project_root = current_file.parent / 'ZEGGS'
+
+print(project_root)
+
+sys.path.append(str(project_root))
+
+
 run()
